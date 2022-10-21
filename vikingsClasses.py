319f5bd3ef9247a88c5453c2e0b1b1466232b49b
fjgarciaes <fj.garciaesurjc@gmail.com>
2022-10-21 17:11:43 +0200

    falta terminar war

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,19 +3,49 @@
 
 
 class Soldier:
-    pass
+    def __init__ (self, health, strength):
+        
+        self.health=health
+        self.strength=strength
+        
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__ (self, name, health, strength):
+        self.name=name
+        self.health=health
+        self.strength=strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if health == 0:
+            return 'NAME has died in act of combat'
+        else:
+            return 'NAME has received DAMAGE points of damage'
+    
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__ (self, health, strength):
+        self.health=health
+        self.strength=strength
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health == 0:
+            return "A Saxon has died in combat"
+        else:
+            return "A Saxon has received " + str(damage) + " points of damage" 
 
 # War
 
