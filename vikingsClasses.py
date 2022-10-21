3b9641620964490ba7590940ae199bc5b685ca1a
kuenk <davidcuencaf@gmail.com>
2022-10-21 15:55:11 +0200

    ww1

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,64 @@
 
 
 class Soldier:
-    pass
-
+    def __init__(self, health, strength):
+        self.health=health
+        self.strength=strength
+        
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health-=damage
+    
+        
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):  
+        self.health=health
+        self.strength=strength
+        self.name=name
+        
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:            
+            return self.name + ' has received ' + str(damage) + ' points of damage'
+        else:
+            return self.name + ' has died in act of combat'             
 
+
+    def battleCry(self):
+        battleCry=('Odin Owns You All!')
+        return battleCry
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        self.health=health
+        self.strength=strength
+    
+    def receiveDamage(self,damage):
+        self.health-=damage
+        if self.health > 0:
+            return 'A Saxon has received ' + str(damage) + ' points of damage'
+        else:
+            return 'A Saxon has died in combat'
 
 # War
 
 
 class War:
-    pass
+    
+    vikingArmy=[]
+    saxonArmy=[]
+    
+    def addViking(Viking):
+        Viking.__init__(self, name, health, strength)
+            if len(vikingArmy) < 10:
+                vikingArmy.append(Viking)
+        
+        
+            
