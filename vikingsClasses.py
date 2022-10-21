cf813138d739cc8b3ff4ff55f947f07182cfed23
Iñigo Larrea <pombo7676@gmail.com>
2022-10-21 16:32:26 +0200

    Lab sin bonus terminado

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,96 @@
 
 
 class Soldier:
-    pass
+    
+    def __init__(self,health,strength):
+        
+        self.health = health
+        self.strength = strength
+        
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage 
 
 # Viking
 
 
-class Viking:
-    pass
-
+class Viking(Soldier):
+    
+    def __init__(self,name,health,strenght):
+        
+        Soldier.__init__(self,health,strenght)
+        
+        self.name = name
+    
+    
+    
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health >0:
+            result = self.name + ' has received ' + str(damage) + ' points of damage'
+            return result
+        else:
+            result = self.name +  ' has died in act of combat'
+            return result
+       
+    def battleCry(self):
+        cry = "Odin Owns You All!"
+        return cry
+    
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    
+    def __init__(self,health,strenght):
+        
+        Soldier.__init__(self,health,strenght)
+        
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health >0:
+            result = "A Saxon has received " +str(damage)+ " points of damage"
+            return result
+        else:
+            result = 'A Saxon has died in combat'
+            return result 
+    
+    
+    
 
 # War
 
 
 class War:
-    pass
+    
+    def __init__(self):
+        
+        
+    vikingArmy = []
+    saxonArmy = []
+    
+    def addViking(self,viking):
+        vikingArmy.append(viking)
+    
+    
+    def addSaxon(self,saxon):
+        saxonArmy.append(saxon)
+    
+    
+    def vikingAttack()
+    
+    
+    def saxonAttack()
+    
+    
+    def showStatus()
+    
+    
+    
+    
+    
+    
+    
+    
