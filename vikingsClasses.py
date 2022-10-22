0b506ab23b70413a0a622ce5435e54490fcd1253
kuenk <davidcuencaf@gmail.com>
2022-10-22 19:30:21 +0200

    Ahoi

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,6 +1,7 @@
 
 # Soldier
 
+import random
 
 class Soldier:
     def __init__(self, health, strength):
@@ -53,14 +54,43 @@ class Saxon(Soldier):
 
 
 class War:
+    def __init__(self):
     
-    vikingArmy=[]
-    saxonArmy=[]
+        self.vikingArmy= []
+        self.saxonArmy= []        
+        
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)    
     
-    def addViking(Viking):
-        Viking.__init__(self, name, health, strength)
-            if len(vikingArmy) < 10:
-                vikingArmy.append(Viking)
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+    
+    def vikingAttack(self):
+        kinguito=random.choice(self.vikingArmy)
+        joncito=random.choice(self.saxonArmy)        
+        combate=joncito.receiveDamage(kinguito.attack())
         
+        if joncito.health<=0:
+            self.saxonArmy.remove(joncito)
+          
         
-            
+        return combate
+    
+    def saxonAttack(self):
+        kinguito=random.choice(self.vikingArmy)
+        joncito=random.choice(self.saxonArmy)
+        combate=kinguito.receiveDamage(joncito.attack())
+        
+        if kinguito.health<=0:
+            self.vikingArmy.remove(kinguito)
+        
+        return combate
+    
+    def showStatus(self):
+        if len(self.vikingArmy)>0 and len(self.saxonArmy)>0:
+            return "Vikings and Saxons are still in the thick of battle."
+        elif len(self.vikingArmy)==0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy)==0:
+            return "Vikings have won the war of the century!"
+                            
