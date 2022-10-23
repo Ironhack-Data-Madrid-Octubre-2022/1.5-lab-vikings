f4ddb2badcbb9ead3f4c65ba7465038eed7e8044
Sergio Suarez <superen2000@yahoo.es>
2022-10-23 22:58:29 +0200

    Ultima actualizacion

--- file.py
+++ file.py
@@ -1,6 +1,11 @@
 from vikingsClasses import *
 
 def insertar_vikingos(*args):
+    '''
+    Metodo que inserta una lista de vikingos
+    
+    '''
+    
     nombre=""
     salud=0
     fuerza=0
@@ -28,6 +33,11 @@ def insertar_vikingos(*args):
             
 def insertar_sajones(*args):
     
+    '''
+    Metodo que inserta una lista de vikingos
+    
+    '''
+    
     salud=0
     fuerza=0
     
@@ -39,62 +49,55 @@ def insertar_sajones(*args):
             
             for n in ex.keys():
                 salud=ex['health']
-                print("salud",salud)
                 
                 fuerza=ex['strength']
-                print("fuerza",fuerza)
                 
-            
             sajon=Saxon(salud, fuerza)
             guerra.addSaxon(sajon)
             
-        print("recorre sajones", [i for i in guerra.saxonArmy])
+        
             
     return guerra.saxonArmy
             
 
-def combate(vikingo, sajones, turno):
+def combate(vikingos, sajones, turno):
+    
+    '''
+    Metodo para que combatan dos listas de vikingos y sajones
+    
+    '''
     guerra=War()
-    while(turno!=0):
-        
+    for v in lista_vikingos:
+        guerra.addViking(v)
+    for s in lista_sajones:
+        guerra.addSaxon(s)
+    
+    accion=turno
+    for v in range(turno):
+        print("El turno es", accion)
+        if len(guerra.saxonArmy)==0 or len(guerra.vikingArmy)==0:
+            return guerra.showStatus()
+            
         guerra.vikingAttack()
+        if len(guerra.saxonArmy)==0:
+            return guerra.showStatus()
         guerra.saxonAttack()
-        
-        turno-=1
+        if len(guerra.vikingArmy)==0:
+            return guerra.showStatus()
+        print("Ejercito saxon",len(guerra.saxonArmy))
+        print("Ejercito viking",len(guerra.vikingArmy))
+      
+        turno-=1    
+        accion-=1
         
     return guerra.showStatus()
 
+#Listas de prueba
 
-
-        
-        
-        
-        
-
-                
-                
-                
-                
-                #vikingo=Viking(n,h)
-                #guerra.addViking
-            
-    
-    
-
-            
-            
-
-            
-        
-        
-        
-        
-        
-
-vikingos1=({"name":"Harald", "health":300, "strength":250},
-       {"name":"Erik", "health":300, "strength":250},
-       {"name":"Gunalf", "health":300, "strength":250},
-       {"name":"Grelda", "health":300, "strength":250})
+vikingos1=({"name":"Harald", "health":300, "strength":500},
+       {"name":"Erik", "health":300, "strength":500},
+       {"name":"Gunalf", "health":300, "strength":500},
+       {"name":"Grelda", "health":300, "strength":500})
 
 sajones1=({ "health":100, "strength":56},
        {"health":300, "strength":250},
@@ -102,18 +105,13 @@ sajones1=({ "health":100, "strength":56},
        { "health":410, "strength":150})
 
 
-
+##Insertamos las listas y las cargamos al combate
 lista_vikingos=insertar_vikingos(vikingos1)
 lista_sajones=insertar_sajones(sajones1)
 
-combate(lista_vikingos,lista_sajones,5)
+print(combate(lista_vikingos,lista_sajones,4))
 
 
 
-
-'''for e in lista:
-    for k in e.values():
-        print(k)'''
-    
     
         
